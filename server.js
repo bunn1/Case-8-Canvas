c94e907580db97bcb8ab737818928417ea814478
bunn1 <nicjohbun@gmail.com>
2022-10-06 11:49:05 +0200

    listen connection server.js close event server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("hello world!");
+// 'console.log("hello world!");'
+
+import {WebSocketServer} from 'ws';
+
+// Create WebSocket server
+const wss = new WebSocketServer({port: 8081});
+
+// Listen on new connections
+wss.on('connection', (ws) => {
+    console.log("hello world");
+    console.log("New client connections");
+
+    // Websocket events (ws) for single client
+
+    // close event
+    ws.on('close', () => {
+        console.log("Client disconnected")
+    });
+});
