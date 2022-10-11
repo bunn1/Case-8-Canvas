58a474d93390c16303130854e2f67d9b611fe75c
bunn1 <nicjohbun@gmail.com>
2022-10-11 10:29:22 +0200

    create file script createing variables canvas

---
+++ libs/script.js
@@ -0,0 +1,12 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+// Set width and height variable
+let width = (canvas.width = window.innerWidth);
+let height = (canvas.height = window.innerHeight);
+
+// Starting position square
+let x = width / 2;
+let y = height / 2;
+
+l
